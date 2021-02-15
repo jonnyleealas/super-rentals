0771202a59ace13ea2112dee689f636fe1cc87ee
jonathon lee <jonnyleealas@gmail.com>
2021-02-15 12:14:36 -0600

    css progress

---
+++ app/components/guitar-form.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+const {
+    Component,
+    get,
+    set
+} = Ember;
+export default Component.extend({
+    tagName: "form",
+    attributeBindings: ["action"],
+
+    action: "https://www.baeldung.com/java-intro-to-the-play-framework",
+
+    input(){
+        console.log("i am inputting")
+    },
+    submit(event){
+        event.preventDefault();
+
+
+    }
+});
--- app/models/guitar.js
+++ app/models/guitar.js
@@ -4,7 +4,7 @@ export default DS.Model.extend({
     name: DS.attr('string'),
     make: DS.attr('string'),
     model: DS.attr('string'),
-    image: DS.attr(),
+    image: DS.attr('string'),
     yearMade: DS.attr('number'),
     description: DS.attr('string'),
     color: DS.attr('string'),
