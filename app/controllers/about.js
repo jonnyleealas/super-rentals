4651a7aea7579dd87572234c4d1eec39c3365b79
jonathon lee <jonnyleealas@gmail.com>
2021-02-18 23:56:26 -0600

    delete prompt works

--- app/controllers/about.js
+++ app/controllers/about.js
@@ -3,18 +3,17 @@ import Controller from '@ember/controller';
 export default Controller.extend({
     actions: {
         onSubmit(){console.log("some shit in onSubmit")},
-        startNewGuitar(){
+        startNewGuitar () {
             let newGuitar = this.store.createRecord("guitar");
             this.set("newGuitar", newGuitar);
             console.log("new guitar", this.get("newGuitar"));
         },
-        submitNewGuitar(){
+        submitNewGuitar () {
             this.get("newGuitar").save().then(() => {
-                // alerts("Successfully Added Guitar")
                 this.set("newGuitar", null)
+                alert("Successfully Added Guitar")
             })
-
         },
-
+       
     }
 });
---
+++ app/controllers/guitars/show.js
@@ -0,0 +1,12 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+    actions: {
+        deleteGuitar () {
+            alert("deleted a guitar")
+            let destroyId = this.get('destroyId');
+            let destroyGuitar = this.get('model').findById('id', destroyId)
+            destroyGuitar.destroyRecord();
+        }, 
+    }
+});
