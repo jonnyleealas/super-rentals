d87d6c6707b51625dc8e117fa13f948fcacb5967
jonathon lee <jonnyleealas@gmail.com>
2021-02-19 22:03:06 -0600

    functional with added margin bottom

--- app/controllers/guitars/show.js
+++ app/controllers/guitars/show.js
@@ -9,10 +9,25 @@ export default Controller.extend({
             alert("Deleted")
             this.transitionToRoute("guitars")
         });
-    }
-         
-        
-        
+    },
+
+    isEditing(){
+        this.set("isEditing", true)
+    },
+
+    updateGuitar(){
+        this.get("model").save().then(()=>{
+            this.set("isEditing", false)
+        })
+    console.log("update guitar")
+    },
+
+    cancelEdit(){
+        this.get('model').rollbackAttributes();
+        this.set("isEditing", false);
+
+    },
+       
     }
 });
 
