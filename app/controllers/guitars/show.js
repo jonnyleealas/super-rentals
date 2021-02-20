f9719722d0f207d02c789001eb1931733511f60a
jonathon lee <jonnyleealas@gmail.com>
2021-02-19 20:44:14 -0600

    delete works

--- app/controllers/guitars/show.js
+++ app/controllers/guitars/show.js
@@ -3,27 +3,16 @@ import Controller from '@ember/controller';
 export default Controller.extend({
     actions: {
 
-        // deleteGuitar() { 
-        //     console.log("delete guitar")
-        //     let destroyId = this.store.findRecord('model', model.id) 
-        //     console.log("destroyed id:::::",guitar.id)
-        //     let game = this.get('model.id').findById('id', destroyId)  
-        //     game.destroyRecord() 
-        //   },
-
-            deleteGuitar () {
-            // alert("deleted a guitar")
-            let destroyGuitar = this.get('model')
-            console.log("destroyed id:::::", destroyGuitar)
-            // let destroyId = this.store.findRecord('guitar', guitars.guitar_id);
-            destroyGuitar.destroyRecord();
-        },
+        deleteGuitar() { 
+      
+        this.get('model').destroyRecord().then(()=>{
+            alert("Deleted")
+            this.transitionToRoute("guitars")
+        });
+    }
+         
         
-       
-        updateGuitar () {
         
-            // alert("Guitar Updated");
-        }
     }
 });
 
--- app/routes/guitars/show.js
+++ app/routes/guitars/show.js
@@ -2,6 +2,8 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
     model(params) {
+      console.log(params,"fuck")
         return this.store.findRecord('guitar', params.guitar_id);
+
       }
-});
+    });
